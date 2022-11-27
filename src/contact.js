4e20065e94adef2f3ef43cf37566e881599922a0
wesley <wesley_snaine@hotmail.com>
2022-11-27 14:39:30 -0300

    visual changes

--- src/contact.js
+++ src/contact.js
@@ -11,25 +11,29 @@ export default function createContactPage (eventTarget) {
     }
 
     //This will apply styles to the container.
-    container.style.backgroundImage = `linear-gradient(135deg, black 40%, rgba(0, 0, 0, 0) 40%), url(${image})`;
+    container.style.backgroundImage = `linear-gradient(135deg, black 50%, rgba(0, 0, 0, 0) 50%), url(${image})`;
     container.style.backgroundSize = 'cover';
-
+    container.style.backgroundOrigin = 'right';
     //This will create the content of container.
     const contact = document.createElement('section');
     contact.id = 'contact-container';
 
     const frag = document.createDocumentFragment();
     const informations = [
-        ['Phone: +0-123-456-7890'],
-        ['Email: dummyemail@nonexistent.com'],
-        ['Localization: 1234 Undefined, St. Nothing']
+        [['✨ Phone:'], ['   +0-123-456-7890']],
+        [['✨ Email:'], ['   dummyemail@nonexistent.com']],
+        [['✨ Localization:'], ['   1234 Undefined, St. Nothing']]
     ]
     for (let i=0; i<3; i++) {
-        const p = document.createElement('p');
-        p.textContent = informations[i];
-        frag.appendChild(p);
+        const div = document.createElement('div');
+        const title = document.createElement('h3');
+        const content = document.createElement('p');
+        title.textContent = informations[i][0]
+        content.textContent = informations[i][1];
+        div.appendChild(title);
+        div.appendChild(content);
+        frag.appendChild(div);
     }
     contact.appendChild(frag);
-
     container.appendChild(contact);
 }
